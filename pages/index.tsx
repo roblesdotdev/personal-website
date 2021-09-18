40edf459bdaaad4d6b370f8c047f47913fa26f21
roblesdotdev <robles.contactme@gmail.com>
2021-09-18 18:14:32 -0300

    adding tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>It works</h1>
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="font-bold text-2xl text-gray-800">It works</h1>
     </div>
   )
 }
