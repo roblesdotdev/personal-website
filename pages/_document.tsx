7b8cfe7e29d39535d2a722ca93d8a7cae46086ce
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2021-09-20 17:10:16 -0300

    adding google fonts base #1 (#2)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;500;700;900&family=Merriweather:wght@300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body className="antialiased bg-gray-100">
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'serif': ['Merriweather', ...defaultTheme.fontFamily.serif],
+        'sans': ['Fira Sans', ...defaultTheme.fontFamily.sans],
+        /* TODO: ADD MONO FONT */
+      }
+    },
   },
   variants: {
     extend: {},
